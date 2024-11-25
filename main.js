b12e2228d34bc101d055c6447e39a0c550c26cf3
info.cryptocurious <info.cryptocurious@gmail.com>
2024-11-25 01:59:25 +0000

    Parallex Effect using Javascript

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 // Add JS here
+const text = document.getElementById('text');
+const leaf = document.getElementById('leaf');
+const tree = document.getElementById('tree');
+const hill4 = document.getElementById('hill4');
+const hill5 = document.getElementById('hill5');
+
+window.addEventListener('scroll', () => {
+    let value = window.scrollY;
+    text.style.marginTop = `${value * 1.5}px`;
+    leaf.style.marginTop = `${value * -1.5}px`;
+    leaf.style.left = `${value * 1.5}px`;
+    hill4.style.left = `${value * -1.5}px`;
+    hill5.style.left = `${value * 1.5}px`;
+});
